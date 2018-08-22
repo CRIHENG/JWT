a046a18804692a602106f08a97014af55765ee2f
CRIHENG <crh1028vip@163.com>
2018-08-22 11:25:10 +0800

    登陆  登出

--- crh-token-service/src/main/resources/data.sql
+++ crh-token-service/src/main/resources/data.sql
@@ -1,3 +1,2 @@
 
-insert into user (id,username,password,rule)VALUES (1,'admin','123',0);
-insert into user (id,username,password,rule)VALUES (2,'user','123',1);
+insert into user VALUES (1,'admin','123456','2018-08-21','0');
